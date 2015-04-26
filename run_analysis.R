23f6b90ebafd7be79d34c7c138f0f7de6cbb9dab
PythonShell <pythonshell@yeah.net>
2015-04-26 15:16:35 +0800

    the main script
    Signed-off-by: PythonShell <pythonshell@yeah.net>

---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+train_data <- read.table("./dataset/train/X_train.txt")
+train_label <- read.table("./dataset/train/y_train.txt")
+train_subject <- read.table("./dataset/train/subject_train.txt")
+
+test_data <- read.table("./dataset/test/X_test.txt")
+test_label <- read.table("./dataset/test/y_test.txt")
+test_subject <- read.table("./dataset/test/subject_test.txt")
+
+binded_data <- rbind(train_data, test_data)
+binded_label <- rbind(train_label, test_label)
+binded_subject <- rbind(train_subject, test_subject)
+
+features <- read.table("./dataset/features.txt")
+index <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+binded_data <- binded_data[, index]
+names(binded_data) <- gsub("\\(\\)", "", features[index, 2])
+names(binded_data) <- gsub("-", "_", names(binded_data))
+
+activity <- read.table("./dataset/activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+activity_label <- activity[binded_label[, 1], 2]
+binded_label[, 1] <- activity_label
+names(binded_label) <- "activity"
+
+names(binded_subject) <- "subject"
+cleaned_data <- cbind(binded_subject, binded_label, binded_data)
+write.table(cleaned_data, "merged_data.txt")
+
+subject_len <- length(table(binded_subject))
+activity_len <- dim(activity)[1]
+column_len <- dim(cleaned_data)[2]
+result <- matrix(NA, nrow=subject_len*activity_len, ncol=column_len) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(cleaned_data)
+
+row_num <- 1
+for(i in 1:subject_len) {
+  for(j in 1:activity_len) {
+    result[row_num, 1] <- sort(unique(binded_subject)[, 1])[i]
+    result[row_num, 2] <- activity[j, 2]
+    logic_1 <- i == cleaned_data$subject
+    logic_2 <- activity[j, 2] == cleaned_data$activity
+    result[row_num, 3:column_len] <- colMeans(cleaned_data[logic_1&logic_2, 3:column_len])
+    row_num <- row_num + 1
+  }
+}
+
+write.table(result, "output_data.txt", row.name=FALSE)
