04aa5793e40f94a8ca68b69532cb7beb8d3378ee
PythonShell <pythonshell@yeah.net>
2015-04-26 15:43:25 +0800

    add several comments
    Signed-off-by: PythonShell <pythonshell@yeah.net>

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,28 @@
+# Reading data from X_train.txt, y_train.txt and subject_train.txt
+# in the subfolder dataset/train
 train_data <- read.table("./dataset/train/X_train.txt")
 train_label <- read.table("./dataset/train/y_train.txt")
 train_subject <- read.table("./dataset/train/subject_train.txt")
 
+# Reading data from X_test.txt, y_test.txt and subject_test.txt
+# in the subfolder dataset/train
 test_data <- read.table("./dataset/test/X_test.txt")
 test_label <- read.table("./dataset/test/y_test.txt")
 test_subject <- read.table("./dataset/test/subject_test.txt")
 
+# Bind two data frame into one data frame called binded_data
 binded_data <- rbind(train_data, test_data)
 binded_label <- rbind(train_label, test_label)
 binded_subject <- rbind(train_subject, test_subject)
 
+# Read features from features.txt and edit the names
 features <- read.table("./dataset/features.txt")
 index <- grep("mean\\(\\)|std\\(\\)", features[, 2])
 binded_data <- binded_data[, index]
 names(binded_data) <- gsub("\\(\\)", "", features[index, 2])
 names(binded_data) <- gsub("-", "_", names(binded_data))
 
+# Read and make used of activity_labels.txt
 activity <- read.table("./dataset/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
@@ -24,10 +31,12 @@ activity_label <- activity[binded_label[, 1], 2]
 binded_label[, 1] <- activity_label
 names(binded_label) <- "activity"
 
+# Write cleaned data
 names(binded_subject) <- "subject"
 cleaned_data <- cbind(binded_subject, binded_label, binded_data)
 write.table(cleaned_data, "merged_data.txt")
 
+# clean result's name
 subject_len <- length(table(binded_subject))
 activity_len <- dim(activity)[1]
 column_len <- dim(cleaned_data)[2]
